4bd219a141aa4c85f0e871df5d78b1b44834761d
Collier Studios <collierjw93@gmail.com>
2017-05-16 12:24:11 -0400

    Add files via upload

--- TicTacTow/src/Button.java
+++ TicTacTow/src/Button.java
@@ -7,30 +7,29 @@ public class Button extends JButton implements ActionListener{
 
 	//The two options asside from empty
 	ImageIcon  X,O;
-	
-	
-	int turn = 0;
+
 	public Button()
 	{
 		X = new ImageIcon(this.getClass().getResource("x.jpg"));
 		O = new ImageIcon(this.getClass().getResource("o.jpg"));
 		this.addActionListener(this);
-	}
-	
-	public void actionPerformed(ActionEvent e)
-	{
 		
-		switch(turn)
+	}
+	public void actionPerformed(ActionEvent event)
+	{
+	
+		switch(TicTacToe.turn)
 		{
 		case 0:
 			setIcon(X);
+			TicTacToe.turn ++;
 			break;
 		case 1:
 			setIcon(O);
+			TicTacToe.turn --;
 			break;
 		}
-		turn ++;
-		turn %=2;
+		
 		
 	}
 }
--- TicTacTow/src/TicTacToe.java
+++ TicTacTow/src/TicTacToe.java
@@ -3,22 +3,29 @@ import javax.swing.JFrame;
 import java.awt.GridLayout;
 
 public class TicTacToe extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	JPanel panel = new JPanel();
+	
+	//turn counter called in the button to switch between x and o
+	public static int turn = 0;
 	
 	//the brackets are creating an array
 	//we need nine buttons so a total of 9 slots for buttons in the array
-	
-	Button button[] = new Button[9];
+	Button[] button = new Button[9];
 	
 	public static void main(String[] args) {
 		new TicTacToe();
+		
 	}
 	
 	public TicTacToe()
 	{
 		//making it so each square is 600x600
 		//can be resized, maybe change that?
-		//make the prpgram stop when closed
+		//make the program stop when closed
 		
 		setSize(1000,1000);
 		setResizable(false);
@@ -41,3 +48,4 @@ public class TicTacToe extends JFrame {
 		setVisible(true);
 	}
 }
+
